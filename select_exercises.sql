cd6cb2a090eb4d3e4be77d8523d9006aaa267df9
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-07-27 12:22:09 -0500

    commits part one of basic statements

---
+++ select_exercises.sql
@@ -0,0 +1,16 @@
+USE codeup_test_db;
+
+SELECT * FROM albums
+WHERE artist = 'Pink Floyd';
+
+SELECT release_date FROM albums
+WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
+
+SELECT * FROM albums
+WHERE release_date BETWEEN '1990' AND '1999';
+
+SELECT * FROM albums
+WHERE sales < '20';
+
+SELECT * FROM albums
+WHERE genre = 'Rock';
