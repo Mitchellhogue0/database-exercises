6a3b63eaac58faf1361ced2f29ad1350a7ed0bfd
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-07-28 11:58:57 -0500

    commits where clauses part 2

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,7 +2,7 @@ USE employees;
 
 # Find all employees with first names 'Irena', 'Vidya', or 'Maya'
 SELECT * FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya' AND gender = 'M';
 
 # Find all employees whose last name starts with 'E'
 SELECT * FROM employees
@@ -11,3 +11,15 @@ WHERE last_name LIKE '%e';
 # Find all employees with a 'q' in their last name
 SELECT * FROM employees
 WHERE last_name LIKE '%q%';
+
+# Find all employees whose last name starts or ends with 'E'
+SELECT * FROM employees
+WHERE last_name LIKE '%e' OR 'e%';
+
+# Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E'
+SELECT * FROM employees
+WHERE last_name LIKE '%e' AND last_name LIKE 'e%';
+
+# Find all employees with a 'q' in their last name but not 'qu'
+SELECT * FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE  '%qu%';
