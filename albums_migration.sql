c7a90420695c5ef403c32fa6813dc63829f96d33
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-07-29 14:19:33 -0500

    commits indexes exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -9,5 +9,6 @@ CREATE TABLE albums (
                         release_date  INT NOT NULL,
                         sales  FLOAT NOT NULL,
                         genre  VARCHAR(150) NOT NULL,
-                        PRIMARY KEY(id)
+                        PRIMARY KEY(id),
+                        UNIQUE (artist, name)
 );
--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -2,6 +2,7 @@ USE employees;
 
 # Return 10 employees in a result set named 'full_name' in the format of 'last name, first name' for each employee.
 # Add the date of birth for each employee as 'DOB' to the query.
+# include the employee number so it is formatted as 'employee number - last name, first name'
 SELECT CONCAT(emp_no, ' - ' , last_name, ', ', first_name) as 'full_name', birth_date as 'DOB'
 FROM employees
 LIMIT 10;
