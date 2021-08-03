7cbeff07e8dce7330d7c7669ef109796fb97ebe8
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-08-03 14:37:19 -0500

    Commits additional movie to table

--- movies_db_create_script.sql
+++ movies_db_create_script.sql
@@ -4,6 +4,7 @@ USE movie_db;
 
 DROP TABLE IF EXISTS movies;
 
+
 CREATE TABLE movies
 (
     id     INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -14,10 +15,17 @@ CREATE TABLE movies
     PRIMARY KEY (id)
 );
 
-INSERT INTO movies (id, title, year, rating, plot)
+INSERT INTO movies (title, year, rating, plot)
 VALUES ('Tenet', '2021', '4',
         'Armed with only one word, Tenet, and fighting for the survival of the entire world, a Protagonist journeys
 through a twilight world of international espionage on a mission that will unfold in something beyond real time.'),
        ('Resident Evil', '2002', '10', 'A special military unit fights a powerful, out-of-control supercomputer and
 hundreds of scientists who have mutated into flesh-eating creatures after a laboratory accident.'),
-       ('Shrek 2', '2004', '10', null);
+       ('Shrek 2', '2004', '10', null),
+       ('Troy', '2004', '7', 'Based on Homer''s \"Iliad,\" this epic portrays the battle between the ancient kingdoms
+of Troy and Sparta. While visiting Spartan King Menelaus (Brendan Gleeson), Trojan prince Paris (Orlando Bloom) falls
+for Menelaus'' wife, Helen (Diane Kruger), and takes her back to Troy. Menelaus'' brother, King Agamemnon (Brian Cox),
+having already defeated every army in Greece, uses his brother''s fury as a pretext to declare war against Troy, the
+last kingdom preventing his control over the Aegean Sea.');
+
+SELECT * FROM movies;
